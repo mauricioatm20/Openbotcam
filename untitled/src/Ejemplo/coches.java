79e6588a63a7fc04f5edf9d4530cb492472c87fc
mauricio <mauriciochasi@gmail.com>
2022-11-28 18:31:05 +0100

    ejercicio3

---
+++ untitled/src/Ejemplo/coches.java
@@ -0,0 +1,14 @@
+package Ejemplo;
+
+public class coches {
+    public static void main(String[] args) {
+
+        String[] coches = new String [] {"seat", "audio", "renault"};
+
+        for (int i = 0; i < coches.length; i++){
+            System.out.println(coches[i]);
+        }
+
+
+    }
+}
